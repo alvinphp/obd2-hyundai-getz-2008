0ac067b094b886d08bf6d257280c9401701a6390
Alvin Gil Saldaña <alvingil86@gmail.com>
2026-05-09 18:43:38 -0500

    Update obd_class.py

--- elm327/obd_class.py
+++ elm327/obd_class.py
@@ -1,30 +1,82 @@
 import obd
 
+
 class OBDApp:
 
-    def voltaje_bateria(self, conexion):
-        volt = conexion.query(obd.commands.ELM_VOLTAGE)
+    def __init__(self):
+        self.conexion = None
+        self.conectado = False
 
-        if volt.is_null():
-            return None
+    def conectar(self, puerto="COM5"):
+        try:
+            self.conexion = obd.OBD(puerto)
 
-        return round(volt.value.magnitude, 2)
+            if self.conexion.is_connected():
+                self.conectado = True
+                return True
+            else:
+                self.conexion = None
+                self.conectado = False
+                return False
 
+        except Exception as e:
+            print("Error conexión:", e)
+            self.conexion = None
+            self.conectado = False
+            return False
 
-    def level_fuel(self, conexion):
-        level = conexion.query(obd.commands.FUEL_LEVEL)
+    def desconectar(self):
+        if self.conexion:
+            self.conexion.close()
 
-        if level.is_null():
-            return None
+        self.conexion = None
+        self.conectado = False
 
-        return round(level.value.magnitude, 2)
-    
-    def fuel_status(self, conexion):
-        status = conexion.query(obd.commands.FUEL_STATUS)
+    def safe(self, sensor):
+        try:
+            if sensor is None or sensor.is_null():
+                return 0
+            return sensor.value.magnitude
+        except:
+            return 0
 
-        if status.is_null():
-            return "N/A"
+    def comandos_obd(self):
 
-        txt = str(status.value)
+        if not self.conectado or not self.conexion:
+            return {}
 
-        return txt
+        try:
+            rpm = self.safe(self.conexion.query(obd.commands.RPM))
+            speed = self.safe(self.conexion.query(obd.commands.SPEED))
+            temp = self.safe(self.conexion.query(obd.commands.COOLANT_TEMP))
+            load = self.safe(self.conexion.query(obd.commands.ENGINE_LOAD))
+            throttle = self.safe(self.conexion.query(obd.commands.THROTTLE_POS))
+            pressure = self.safe(self.conexion.query(obd.commands.INTAKE_PRESSURE))
+            stft = self.safe(self.conexion.query(obd.commands.SHORT_FUEL_TRIM_1))
+            ltft = self.safe(self.conexion.query(obd.commands.LONG_FUEL_TRIM_1))
+            fuel_level = self.safe(self.conexion.query(obd.commands.FUEL_LEVEL))
+            voltage = self.safe(self.conexion.query(obd.commands.ELM_VOLTAGE))
+
+            fuel_status_raw = self.conexion.query(obd.commands.FUEL_STATUS)
+
+            fuel_status = "N/A"
+            if fuel_status_raw and not fuel_status_raw.is_null():
+                fuel_status = str(fuel_status_raw.value)
+
+            return {
+                "RPM": rpm,
+                "SPEED": speed,
+                "TEMP": temp,
+                "LOAD": load,
+                "THROTTLE": throttle,
+                "PRESSURE": pressure,
+                "STFT": stft,
+                "LTFT": ltft,
+                "FUEL_LEVEL": fuel_level,
+                "FUEL_STATUS": fuel_status,
+                "VOLTAGE": voltage
+            }
+
+        except Exception as e:
+            print("error OBD:", e)
+            return {}
